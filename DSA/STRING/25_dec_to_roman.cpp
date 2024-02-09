e4d826c414a973369ba1d94c8ddf3048853c5161
shreyas <shreyasg.is21@gmail.com>
2024-02-10 00:58:53 +0530

    DAY REST

--- DSA/STRING/25_dec_to_roman.cpp
+++ DSA/STRING/25_dec_to_roman.cpp
@@ -4,15 +4,19 @@
 using namespace std;
 
 // The given code for the Solution class
-class Solution {
+class Solution
+{
 public:
-    string intToRoman(int num) {
+    string intToRoman(int num)
+    {
         string roman;
         vector<string> notations = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
         vector<int> value = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-        //must note down all the all these values
-        for(int pos = 0; num>0; ++pos){  // to go thru the value vector to compare with num
-            while(num >= value[pos]){  //as num is greater we add the CHAR to roman else we recudes the chechk value of the value vector
+        // must note down all the all these values
+        for (int pos = 0; num > 0; ++pos)
+        { // to go thru the value vector to compare with num
+            while (num >= value[pos])
+            { // as num is greater we add the CHAR to roman else we recudes the chechk value of the value vector
                 roman += notations[pos];
                 num -= value[pos];
             }
@@ -21,15 +25,15 @@ public:
     }
 };
 
-//USING MAPS
-// class Solution {
-// public:
-//     string intToRoman(int num) {
-//         vector<pair<int, string>> map{{1, "I"}, {4, "IV"}, {5, "V"}, {9, "IX"}, {10, "X"}, {40, "XL"}, {50, "L"}, 
-// 		{90, "XC"}, {100, "C"}, {400, "CD"}, {500, "D"}, {900, "CM"}, {1000, "M"}};
-//         int l = map.size()-1;
-//         string s="";
-        
+// USING MAPS
+//  class Solution {
+//  public:
+//      string intToRoman(int num) {
+//          vector<pair<int, string>> map{{1, "I"}, {4, "IV"}, {5, "V"}, {9, "IX"}, {10, "X"}, {40, "XL"}, {50, "L"},
+//  		{90, "XC"}, {100, "C"}, {400, "CD"}, {500, "D"}, {900, "CM"}, {1000, "M"}};
+//          int l = map.size()-1;
+//          string s="";
+
 //         while(num!=0){
 //             while(map[l].first>num){   //do till we find when num>> map.first
 //                 l--;-
@@ -42,7 +46,8 @@ public:
 // };
 
 // The main function
-int main() {
+int main()
+{
     // Create an object of the Solution class
     Solution sol;
 
