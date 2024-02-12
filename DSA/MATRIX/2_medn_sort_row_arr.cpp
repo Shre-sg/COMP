c4b7c202a773b23c1ebd7298364430f3f51ea92b
shreyas <shreyasg.is21@gmail.com>
2024-02-13 02:19:20 +0530

    CODE REVIWQ

--- DSA/MATRIX/2_medn_sort_row_arr.cpp
+++ DSA/MATRIX/2_medn_sort_row_arr.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
 #include <vector>
-#include<bits/stdc++.h>
-#include<climits>
+#include <bits/stdc++.h>
+#include <climits>
 using namespace std;
 
-//WATCH TUF vid
-// Given a row wise sorted matrix of size R*C where R and C are always odd,
-// find the median of the matrix.
+// WATCH TUF vid
+//  Given a row wise sorted matrix of size R*C where R and C are always odd,
+//  find the median of the matrix.
 
 // Example 1:
 
 // Input:
 // R = 3, C = 3
-// M = [[1, 3, 5], 
-//      [2, 6, 9], 
+// M = [[1, 3, 5],
+//      [2, 6, 9],
 //      [3, 6, 9]]
 // Output: 5
-// Explanation: Sorting matrix elements gives 
-// us {1,2,3,3,5,6,6,9,9}. Hence, 5 is median. 
- 
+// Explanation: Sorting matrix elements gives
+// us {1,2,3,3,5,6,6,9,9}. Hence, 5 is median.
 
 // Example 2:
 
@@ -26,46 +25,52 @@ using namespace std;
 // R = 3, C = 1
 // M = [[1], [2], [3]]
 // Output: 2
-// Explanation: Sorting matrix elements gives 
+// Explanation: Sorting matrix elements gives
 // us {1,2,3}. Hence, 2 is median.
 
-// Your Task:  
+// Your Task:
 // You don't need to read input or print anything. Your task is to complete the function median() which takes the integers R and C along with the 2D matrix as input parameters and returns the median of the matrix.
 
 // Expected Time Complexity: O(32 * R * log(C))
 // Expected Auxiliary Space: O(1)
 
-
-class Solution {
+class Solution
+{
 public:
-    int median(vector<vector<int>>& matrix, int R, int C) {
+    int median(vector<vector<int>> &matrix, int R, int C)
+    {
         // Initialize the minimum and maximum values for the binary search range.
         int min_val = INT_MAX;
         int max_val = INT_MIN;
 
         // Find the minimum and maximum values in the matrix.
-        for (int i = 0; i < R; ++i) {
-            min_val = min(min_val, matrix[i][0]);       // Minimum value in the first column
-            max_val = max(max_val, matrix[i][C - 1]);   // Maximum value in the last column
+        for (int i = 0; i < R; ++i)
+        {
+            min_val = min(min_val, matrix[i][0]);     // Minimum value in the first column
+            max_val = max(max_val, matrix[i][C - 1]); // Maximum value in the last column
         }
 
         // Binary search to find the median.
-        while (min_val < max_val) {
-            
-            int mid_val = min_val + (max_val - min_val) / 2;   //max +low /2
-            int count = 0; // Count of elements less than or equal to mid_val
+        while (min_val < max_val)
+        {
+
+            int mid_val = min_val + (max_val - min_val) / 2; // max +low /2
+            int count = 0;                                   // Count of elements less than or equal to mid_val
 
             // Count the number of elements less than or equal to mid_val in each row.
-            for (int i = 0; i < R; ++i) {  //itertaor to ele which is max xof mid  -   ///0 index
+            for (int i = 0; i < R; ++i)
+            { // itertaor to ele which is max xof mid  -   ///0 index
                 count += upper_bound(matrix[i].begin(), matrix[i].end(), mid_val) - matrix[i].begin();
             }
 
             // If count is less than or equal to (R * C) / 2, update the minimum value.
-            if (count <= (R * C) / 2) {
+            if (count <= (R * C) / 2)
+            {
                 min_val = mid_val + 1;
-            } 
+            }
             // If count is greater than (R * C) / 2, update the maximum value.
-            else {
+            else
+            {
                 max_val = mid_val;
             }
         }
@@ -75,14 +80,13 @@ public:
     }
 };
 
-
-int main() {
+int main()
+{
     // Example matrix
     vector<vector<int>> matrix = {
         {1, 3, 5},
         {2, 6, 9},
-        {3, 6, 9}
-    };
+        {3, 6, 9}};
 
     int R = matrix.size();    // Number of rows
     int C = matrix[0].size(); // Number of columns
@@ -98,11 +102,6 @@ int main() {
     return 0;
 }
 
-
-
-
-
-
 // int countEleSmallerEqualToMid(vector<int>& arr,int m,int mid){
 //         int low=0, high=m-1;
 //         while(low<=high){
@@ -115,8 +114,7 @@ int main() {
 //         }
 //         return low;
 //     }
-    
- 
+
 //     int median(vector<vector<int>> &arr, int n, int m){
 //         // code here
 //         int low=1, high=2000;
@@ -136,18 +134,12 @@ int main() {
 //         return low;
 //     }
 
-
-
-
-
-
-
 // Naive solution class Solution {
 //     int median(int matrix[][], int R, int C) {
-//         // code here     
+//         // code here
 //         int [] med = new int[R*C];
 //         int index = 0;
-        
+
 //         for(int i = 0; i<R; i++)
 //         {
 //             for(int j = 0; j< C; j++)
@@ -158,7 +150,6 @@ int main() {
 //         }
 //           Arrays.sort(med);
 //         return med[(R*C)/2];
-        
+
 //     }
 // }
-
